293e0f423089646aba2575a8947495bbd498130b
A PONTEW <giselisantos2019@gmail.com>
2022-03-18 19:50:29 -0300

    codigo completa

---
+++ app.js
@@ -0,0 +1,19 @@
+const input=require("readline-sync")
+
+
+
+function soma(numero1,numero2){
+    return numero1 + numero2
+}
+
+console.log(soma(9,4))
+
+
+
+
+
+function soma2(numero1,numero2){
+    console.log(numero1+numero2)
+}
+
+soma2(2,4) 
